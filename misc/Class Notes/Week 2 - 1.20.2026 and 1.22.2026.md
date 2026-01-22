bdbee85db33f9a7ed22d554f2eda0563d22f03e6
trulycaffeinated <tjeffrey2003@gmail.com>
2026-01-22 10:08:42 -0500

    Added the proper toolboxes

--- misc/Class Notes/Week 2 - 1.20.2026 and 1.22.2026.md
+++ misc/Class Notes/Week 2 - 1.20.2026 and 1.22.2026.md
@@ -33,5 +33,16 @@ Everything in Matlab is an array or matrixed stored as \[Row Col\]
 
 Because everything is a matrix, multiplication is by default matrix multiplication
 - Element by element multiplication is ".\*"
-- Matrix multiplcation is "\*"
+- Matrix multiplication is "\*"
 
+# Peter Corke's Spatial Math Toolbox
+Stereo imaging we focus on a point (P), with two cameras a left ($P_L$) and right ($P_R$)
+From those cameras we an triangulate $Z_P$ using $X_L$ and $X_R$ 
+What happens if $Y_L \neq Y_R$ ? Math gets a lot harder
+- This may happen with cameras outside in the wind
+- **Specifications should include how accurate our system is to *x* amount of wind 
+  (Changes in $\Delta Y$)**
+
+Need to install Peter Corke's...
+- Robotics Toolbox
+- Machine Vision Toolbox
--- misc/Labs/Lab1/lab1.m
+++ misc/Labs/Lab1/lab1.m
@@ -1,32 +1,51 @@
 % Hard coded parameters
 b = 50; % baseline [mm]
-f = 6; % focal length [mm]
+f = 10; % focal length [mm]
 ps = 0.006; % pixel size [mm]
 xNumPix = 752; % number of pixels in the x direction of the sensor [px]
 cxLeft = xNumPix/2; % left camera x center [px]
 cxRight = xNumPix/2; % right camera x center [px]
 
-% Create the ball
-BALL = SE3(50, 0, 100);
-
 % create two cameras
 Cam_L = SE3(0, 0, 0);
 Cam_R = SE3(b, 0, 0);
 
+% Create the ball
+Z_b = linspace(.5, 10, 100); %
+BALL = repmat(SE3(), 1, numel(Z_b));   % preallocate exactly 100
+for i = 1:numel(Z_b)
+    BALL(i) = SE3(0, 0, Z_b(i));
+end
+    
 % see the ball
 BALL_L = inv(Cam_L) * BALL;
 BALL_R = inv(Cam_R) * BALL;
 
 % find the balls position from the cameras
-XL = BALL_L.t(1);
-ZL = BALL_L.t(3);
+pL = transl(BALL_L).';
+pR = transl(BALL_R).';
 
-XR = BALL_R.t(1);
-ZR = BALL_R.t(3);
+XL = pL(1,:)
+ZL = pL(3,:)
 
-xLeft = cxLeft + (f *(XL/ZL)) / ps;
-xRight = cxRight + (f *(XR/ZR)) / ps;
+XR = pR(1,:)
+ZR = pR(3,:)
+
+xLeft = cxLeft + (f *(XL./ZL))
+xRight = cxRight + (f *(XR./ZR))
 
 %Equations to find depth
-d = (abs((xLeft - cxLeft)-abs(xRight-cxRight))*ps);
-Z = (b * f)/d;
+d = abs((xLeft - cxLeft)-(xRight-cxRight))*ps;
+Z = (b * f)./d;
+fprintf("d = %f , Z = %f", d, Z);
+
+% Debug sizes (very helpful)
+fprintf("size(Z_b) = %s\n", mat2str(size(Z_b)));
+fprintf("size(d)   = %s\n", mat2str(size(d)));
+
+figure;
+plot(Z_b(:), d(:), 'LineWidth', 1.5);
+xlabel('True depth Z (mm)');
+ylabel('Disparity d (mm)');
+title('Disparity vs Depth');
+grid on;
