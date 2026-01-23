9ca8eec80b13d64cc49eee12bebf917c1036289b
trulycaffeinated <tjeffrey2003@gmail.com>
2026-01-22 22:50:07 -0500

    Added additional lab 1 script to test multiple focal points

--- misc/Labs/Lab1/Lab1.m
+++ misc/Labs/Lab1/Lab1.m
@@ -1,5 +1,11 @@
 % ------- lab 1 ---------- %
-% default values from LAB
+%
+%   Jeffrey Taylor
+%   CPET 563 - ESD : Capstone
+%   Lab 1 - Stereo Imaging
+%
+% ------- lab 1 ---------- %
+% default values from LAB document
 b = 50; % baseline [mm]
 f6 = 6; % focal length [mm]
 f10 = 10; % secondary focal length
@@ -9,14 +15,17 @@ cxLeft = xNumPix/2; % left camera x center [px]
 cxRight = xNumPix/2; % right camera x center [px]
 
 % The "Ball" or Point (P) which is now moving
-% P = [1, 0, 10]; % Stationary ball
-Z_P = linspace(.1, 10, 100);
+%   The code is formatted here as such so that any change to Z_P (point
+%   depth) will "just work", and no other code will need to be changed to
+%   adjust for either the increased size, or increased number of points.
+Z_P = linspace(.1, 10, 100); % Depth values (.1 -> 10 in 100 increments)
 N = numel(Z_P);
 P = [   ones(1,N);
         zeros(1,N);
-        Z_P];
+        Z_P]; % Moving Point
 
 % ============ f6 ================ %
+% Focal Length = 6
 % Create the left camera
 camLeft_f6=CentralCamera( ...
     'focal',f6/1000, ...
@@ -40,15 +49,17 @@ Tcam_Right_f6=SE3(+b/1000, 0, 0);
 camRight_f6.T=Tcam_Right_f6;
 % Left Camera project
 uv_Left_f6 = camLeft_f6.project(P);
-%%%camLeft_f6.plot(P);
+%%%camLeft_f6.plot(P); % Plot of what the left camera sees
 xLeft_f6 = uv_Left_f6(1, :)
 
 % Right Camera Project
 uv_Right_f6 = camRight_f6.project(P);
-%%%camRight_f6.plot(P);
+%%%camRight_f6.plot(P); % Plot of what the right camera sees
 xRight_f6 = uv_Right_f6(1, :)
+% ============ f6 ================ %
 
 % ============ f10 ================ %
+% Focal Length = 10
 % Create the left camera
 camLeft_f10=CentralCamera( ...
     'focal',f10/1000, ...
@@ -73,13 +84,14 @@ camRight_f10.T=Tcam_Right_f10;
 
 % Left Camera project
 uv_Left_f10 = camLeft_f10.project(P);
-%%%camLeft_f10.plot(P);
+%camLeft_f10.plot(P);
 xLeft_f10 = uv_Left_f10(1, :)
 
 % Right Camera Project
 uv_Right_f10 = camRight_f10.project(P);
 %%%camRight_f10.plot(P);
 xRight_f10 = uv_Right_f10(1, :)
+% ============ f10 ================ %
 
 % calculate disparity
 d_f6 = (abs((xLeft_f6-cxLeft)-(xRight_f6-cxRight))*ps) % disparity [mm]
@@ -88,11 +100,11 @@ d_f10 = (abs((xLeft_f10-cxLeft)-(xRight_f10-cxRight))*ps) % disparity [mm]
 %calculate depth
 Z_f6 = (b * f6)./d_f6 % depth [mm]
 Z_m_f6 = Z_f6 ./ 1000 % depth [m]
-Z_m_error_f6 = Z_P - Z_m_f6
+Z_m_error_f6 = Z_P - Z_m_f6 % depth error [m]
 
 Z_f10 = (b * f10)./d_f10 % depth [mm]
 Z_m_f10 = Z_f10 ./ 1000 % depth [m]
-Z_m_error_f10 = Z_P - Z_m_f10
+Z_m_error_f10 = Z_P - Z_m_f10 % depth error [m]
 
 % Plot disparity
 figure
