f8565c79d4347d7f71750b112163e0a0ef3ead25
trulycaffeinated <tjeffrey2003@gmail.com>
2026-04-14 10:29:29 -0400

    Working on updating real time actual ball cords. HSV Method fully implemented

--- misc/Labs/Lab4/blenderServer.py
+++ misc/Labs/Lab4/blenderServer.py
@@ -114,7 +114,7 @@ def handle_data():
     text = namebuf.split(b'\x00', 1)[0].decode("utf-8")
 
     # --- move object ---
-    xform_object_by_name(text, floats[2], floats[3], 3,
+    xform_object_by_name(text, floats[2], floats[3], floats[4],
                               floats[5], floats[6], floats[7])
 
     # --- render both cams ---
